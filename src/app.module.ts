65730327219e940dfd32d3136418d3b6123c365b
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2021-03-01 12:59:05 +0900

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,54 @@
 import { Module } from '@nestjs/common';
+
+import * as Joi from 'joi';
+
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { UsersModule } from './users/users.module';
+import { CafesModule } from './cafes/cafes.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { UploadsModule } from './uploads/uploads.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.prod.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev', 'prod')
+          .valid('dev', 'prod', 'test')
+          .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    UsersModule,
+    CafesModule,
+    ReviewsModule,
+    CommonModule,
+    AuthModule,
+    UploadsModule,
+  ],
 })
 export class AppModule {}
