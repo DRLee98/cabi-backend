49c2df793146689a0af9d7fff9fa1afc119bea70
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2021-06-13 16:34:11 +0900

    DB URL connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,11 +35,11 @@ import { Message } from './talk/entites/message.entity';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.dev.env' : '.prod.env',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.string().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
+        DB_HOST: Joi.string(),
+        DB_PORT: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_NAME: Joi.string(),
         PRIVATE_KEY: Joi.string().required(),
       }),
     }),
@@ -56,11 +56,16 @@ import { Message } from './talk/entites/message.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          }),
+      ssl: { rejectUnauthorized: false },
       entities: [
         User,
         Address,
